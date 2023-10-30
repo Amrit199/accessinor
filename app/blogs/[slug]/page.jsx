ea5bb5a92ac983626dafae2dc838b2a7d69552a4
Amrit <tamangamrit37@gmail.com>
2023-10-30 23:38:52 +0100

    update

--- app/blogs/[slug]/page.jsx
+++ app/blogs/[slug]/page.jsx
@@ -1,59 +1,53 @@
 "use client";
-// import { getPost } from "@/sanity/sanity-utils";
-// import { PortableText } from "@portabletext/react";
-// import Image from "next/image";
-
-// const serilaizers = {
-//   types: {
-//     image: (props) => {
-//       return (
-//         <Image
-//           src={props.node.asset.url}
-//           alt={props.node.alt}
-//           width={props.node.asset.metadata.dimensions.width}
-//           height={props.node.asset.metadata.dimensions.height}
-//         />
-//       );
-//     },
-//   },
-// };
-
-// export default async function Page({ params }) {
-//   const slug = params.slug;
-//   const blog = await getPost(slug);
-
-//   if (!blog) {
-//     return <div className="pt-28 pb-8">Loading...</div>;
-//   }
-//   return (
-//     <div className="w-full pt-24 pb-6 text-black bg-slate-50">
-//       <div className="w-full bg-green-200 p-8 md:p-16">
-//         <h2 className="text-2xl md:text-5xl font-bold">{blog.title}</h2>
-//       </div>
-//       <div className="w-full p-8 md:p-16 lg:px-24 flex flex-col items-start justify-start gap-5">
-//         <p>{blog.intro}</p>
-//         <Image
-//           width={380}
-//           height={350}
-//           src={blog.mainImage}
-//           className="rounded-lg w-auto md:max-w-2xl self-center"
-//           alt={blog.mainImagealt}
-//         />
-//       </div>
-//       <PortableText value={blog.body} serializers={serilaizers}/>
-//     </div>
-//   );
-// }
-
 import { getPost } from "@/sanity/sanity-utils";
 import { PortableText } from "@portabletext/react";
+import { getImageDimensions } from "@sanity/asset-utils";
+import urlBuilder from "@sanity/image-url";
 import Image from "next/image";
+import { useEffect, useState } from "react";
 
-export default function BlogPage({ blog }) {
+const SampleImageComonent = ({ value, isInLine }) => {
+  const { width, height } = getImageDimensions(value);
+  const imageUrl = urlBuilder({ projectId: "7jdr2vdm", dataset: "production" })
+    .image(value)
+    .width(isInLine ? 100 : 800)
+    .fit("max")
+    .auto("format")
+    .url();
+  return (
+    <div className="rounded-lg w-full sm:w-[80%] lg:w-[50%] mx-auto self-center">
+      <Image
+        src={imageUrl}
+        alt={value.alt || " "}
+        width={width}
+        height={height}
+        loading="lazy"
+        layout="responsive"
+      />
+    </div>
+  );
+};
+
+export default function Page({ params }) {
+  const [blog, setBlog] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const post = await getPost(params.slug);
+      setBlog(post);
+    };
+    fetchData();
+  }, [params.slug]);
+
+  const components = {
+    types: {
+      image: SampleImageComonent,
+    },
+  };
+
   if (!blog) {
     return <div className="pt-28 pb-8">Loading...</div>;
   }
-
   return (
     <div className="w-full pt-24 pb-6 text-black bg-slate-50">
       <div className="w-full bg-green-200 p-8 md:p-16">
@@ -68,85 +62,8 @@ export default function BlogPage({ blog }) {
           className="rounded-lg w-auto md:max-w-2xl self-center"
           alt={blog.mainImagealt}
         />
+        <PortableText value={blog.body} components={components} />
       </div>
-      <PortableText value={blog.body} />
     </div>
   );
 }
-
-export async function getStaticProps({ params }) {
-  const blog = await getPost(params.slug);
-
-  if (!blog) {
-    return {
-      notFound: true,
-    };
-  }
-
-  return {
-    props: {
-      blog,
-    },
-  };
-}
-
-export async function getStaticPaths() {
-  // Fetch all the slugs of your posts from Sanity
-  // Replace this with your actual logic
-  const allSlugs = await getAllPostSlugs();
-  const paths = allSlugs.map(slug => ({
-    params: { slug },
-  }));
-
-  return {
-    paths,
-    fallback: true,
-  };
-}
-
-
-// import { useEffect, useState } from "react"
-
-// const Page = ({ params }) => {
-//   const post = params.slug
-//   const blog = getPost(post)
-//   // const [blog, setBlog] = useState(null)
-//   console.log(blog)
-//   // useEffect(() => {
-//   //   if(post) {
-//   //     const fetchedPost = posts.find((blog) => blog.slug == post);
-//   //     setBlog(fetchedPost)
-//   //   }
-//   // }, [post])
-
-//   if (!blog) {
-//     return <div className="pt-28 pb-8">Loading...</div>;
-//   }
-//     return (
-//       <div className="w-full pt-24 pb-6 text-black bg-slate-50">
-//         <div className="w-full bg-green-200 p-8 md:p-16">
-//           <h2 className="text-2xl md:text-5xl font-bold">{blog.title}</h2>
-//           {/* <div className="flex items-center justify-start gap-4 mt-4 md:mt-8">
-//           {blog.body.map((tag, index) => (
-//             <span key={index} className="bg-green-900 text-white px-3 py-1 rounded-xl md:text-lg">{tag}</span>
-//           ))}
-//           </div> */}
-//         </div>
-//         <div className="w-full p-8 md:p-16 lg:px-24 flex flex-col items-start justify-start gap-5">
-//           <p>{blog.intro}</p>
-//           <Image
-//             src={blog.mainImage}
-//             className="rounded-lg w-auto md:max-w-2xl self-center"
-//             alt={blog.mainImagealt}
-//           />
-//           {/* <h3 className="text-xl font-bold">{blog.contentTitle}</h3>
-//           <p>{blog.contentdesc}</p>
-//           <h3 className="text-xl font-bold">{blog.contentTitle1}</h3>
-//           <p>{blog.contentdesc1}</p> */}
-//         </div>
-//         <GetStarted />
-//     </div>
-//     )
-//   }
-
-// export default Page
