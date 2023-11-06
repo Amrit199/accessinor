00d8eadc5c881d1123fe0ace6e2b1a97e1d82eed
Amrit Tamang <44949877+Amrit199@users.noreply.github.com>
2023-11-06 15:49:48 +0100

    services sanity updated

--- sanity.config.js
+++ sanity.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from "sanity";
 import { deskTool } from "sanity/desk"
 import post from "./sanity/schemas/post";
 import { visionTool } from "@sanity/vision"
+import service from "./sanity/schemas/service";
 const config = defineConfig({
     projectId: process.env.NEXT_PUBLIC_PROJECT_KEY,
     dataset: process.env.NEXT_PUBLIC_DATASET_KEY,
@@ -9,7 +10,7 @@ const config = defineConfig({
     apiVersion: '2023-10-25',
     basePath: "/admin",
     plugins: [deskTool(), visionTool({defaultApiVersion: '2023-10-25'})],
-    schema: { types: [post]}
+    schema: { types: [post, service]}
 })
 
 export default config
---
+++ sanity/schemas/service.js
@@ -0,0 +1,45 @@
+const service = {
+  name: "service",
+  title: "services",
+  type: "document",
+  fields: [
+    {
+      name: "title",
+      title: "Title",
+      type: "string",
+    },
+    {
+      name: "slug",
+      title: "Slug",
+      type: "slug",
+      options: { source: "title" },
+    },
+    {
+      name: "intro",
+      title: "Intro",
+      type: "string",
+    },
+    {
+      name: "description",
+      title: "Description",
+      type: "string",
+    },
+    {
+      name: "image",
+      title: "Image",
+      type: "image",
+      options: {
+        hotspot: true,
+      },
+      fields: [
+        {
+          name: "alt",
+          title: "Alt",
+          type: "string",
+        },
+      ],
+    },
+  ],
+};
+
+export default service;
